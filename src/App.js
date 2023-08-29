75321b1ed46b53902805af322267b3e6fd0d7b60
Kyle Bebeau <kyle@theinitgroup.com>
2023-08-28 23:40:39 -0400

    add google tracking and update social cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import Footer from './footer';
+import React from 'react';
 
-import globe from './assets/img/websites/globe_bg.jpg';
 import realeyez from './assets/img/websites/realeyez_bg.jpg';
 import customNature from './assets/img/websites/customNature_bg.jpg';
 import ap from './assets/img/websites/ap_bg.jpg';
@@ -9,41 +7,29 @@ import smokeBreak from './assets/img/websites/smokebreak_bg.jpg';
 import paakhouse from './assets/img/websites/paakhouse_bg.jpg';
 import fiveStar from './assets/img/websites/5star_bg.jpg';
 import dreamworks from './assets/img/websites/dreamworks_bg.jpg';
-import coonguard from './assets/img/websites/coonguard_bg.jpg';
 import traitmarketplace from './assets/img/websites/traitmarketplace_bg.jpg';
 import pgmoutfitters from './assets/img/websites/pgm-outfitters.jpg';
-
-import resume from './assets/docs/kylebebeau_resume.pdf';
-import {Logos, Posters, Prints} from './artwork';
 
 import {ReactComponent as Logo} from './assets/img/cltdev.svg';
 
 const Homepage = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [selectedImage, setSelectedImage] = useState('');
 
-  useEffect(() => {
-    if(!showModal) {
-      setSelectedImage('');
-    }
-  }, [showModal]);
+  const logAnalytics = async () => {
+    window.gtag('config', 'AW-11171481429');
+    window.gtag('event', 'inquiry_click', {
+      send_to: 'AW-11313458751/yymXCNWButgYEL_c1pIq',
+    })
+  }
 
-  useEffect(() => {
-    if(selectedImage !== '') {
-      setShowModal(true);
-    }
-  }, [selectedImage])
+  const handleBtnClick = async () => {
+    await logAnalytics();
+    window.location.href = 'mailto:kylebebeau@gmail.com?subject=Creative Service Request';
+  }
+
   return (
-    <>
-    <div id="modal" className={showModal ? 'show' : null} onClick={() => setShowModal(false)}>
-      <div className="modalBody">
-        <img src={selectedImage} alt="" />
-      </div>
-    </div>
     <div id="home">
 
       <section id="intro">
-        {/* <div id="banner"></div> */}
         <Logo className="logo" />
         <blockquote>
           <p>
@@ -56,19 +42,14 @@ const Homepage = () => {
             We are CLT devs.
           </p>
           <p>Below are links to live client projects.</p>
-          <cite />
         </blockquote>
-        <a href="mailto:kylebebeau@gmail.com?subject=Creative Service Request" className="btn">Get A Quote</a>
+        <button className="btn" onClick={() => handleBtnClick()}>Get A Quote</button>
       </section>
 
       <section id="websites">
-        {/* <h3>Development</h3> */}
         <a href="http://fyc.dreamworks.com" target="_BLANK" rel="noopener noreferrer">
           <img src={dreamworks} alt="Dreamworks FYC 2021" />
         </a>
-        {/* <a href="https://www.npmjs.com/package/@bebeau/globe" target="_BLANK" rel="noopener noreferrer">
-          <img src={globe} alt="Globe Package" />
-        </a> */}
         <a href="https://andersonpaak.com" target="_BLANK" rel="noopener noreferrer">
           <img src={ap} alt="Anderson .Paak" />
         </a>
@@ -93,44 +74,9 @@ const Homepage = () => {
         <a href="http://realeyezapparel.com" target="_BLANK" rel="noopener noreferrer">
           <img src={realeyez} alt="Realeyez" />
         </a>
-        {/* <a href="https://texasbanditcoonguard.com/" target="_BLANK" alt="Texas Bandit Coon Guard" rel="noopener noreferrer">
-          <img src={coonguard} alt="Texas Bandit Coon Guard" />
-        </a> */}
       </section>
-
-      {/* <section id="logos">
-        <h3>Logos</h3>
-        {Logos.map((image, index) => {
-          return(
-            <article>
-              <img key={`logo-${index}`}src={image} alt="" onClick={() => setSelectedImage(image)} />
-            </article>
-          )
-        })}
-      </section>
-
-      <section id="artwork">
-        <h3>Artwork</h3>
-        {Posters.map((image, index) => {
-          return(
-            <article>
-              <img key={`logo-${index}`}src={image} alt="" onClick={() => setSelectedImage(image)} />
-            </article>
-          )
-        })}
-        {Prints.map((image, index) => {
-          return(
-            <article className="thirds">
-              <img key={`logo-${index}`}src={image} alt="" onClick={() => setSelectedImage(image)} />
-            </article>
-          )
-        })}
-      </section> */}
-
-      {/* <Footer /> */}
 
     </div>
-    </>
   )
 }
 
