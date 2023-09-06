94ac4994b3a0005e569e41a5fbb562fa7f6e3a4c
Kyle Bebeau <kyle@theinitgroup.com>
2023-09-05 22:17:47 -0400

    add cityscape video

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 
 import realeyez from './assets/img/websites/realeyez_bg.jpg';
 import customNature from './assets/img/websites/customNature_bg.jpg';
@@ -11,9 +11,13 @@ import traitmarketplace from './assets/img/websites/traitmarketplace_bg.jpg';
 import pgmoutfitters from './assets/img/websites/pgm-outfitters.jpg';
 
 import {ReactComponent as Logo} from './assets/img/logo.svg';
+import webm from './assets/video/clt.webm';
+// import mp4 from './assets/video/clt.mp4';
 
 const Homepage = () => {
-
+  const webRef = useRef();
+  const [showInquiry, setShowInquiry] = useState(false);
+  
   const logAnalytics = async () => {
     window.gtag('config', 'AW-11171481429');
     window.gtag('event', 'inquiry_click', {
@@ -22,27 +26,37 @@ const Homepage = () => {
   }
 
   const handleBtnClick = async () => {
+    // setShowInquiry(true);
     await logAnalytics();
     window.location.href = 'mailto:kylebebeau@gmail.com?subject=Creative Service Request';
+  }
+  
+  const handleArrowClick = () => {
+    webRef.current.scrollIntoView() 
   }
 
   return (
     <div id="home">
 
-      <section id="intro">
+      <video preload='metadata' autoPlay muted loop id="video">
+        <source src={webm} type="video/webm"></source>
+      </video>
+
+      <section id="intro" className={showInquiry ? 'open' : ''}>
         <Logo />
-        <blockquote>
-            We are two Charlotte, NC based developers. 
-            We aim to please, but shoot to kill. 
-            We are the artists, the problem solvers, the software engineers you've been looking for. We are creative guns for hire.
+        <blockquote> 
+          We are creative guns for hire.
+          We aim to please, but shoot to kill. 
+          We are the artists, the problem solvers, the software engineers you've been looking for.
+          We are Charlotte, NC developers.
         </blockquote>
         <button className="btn" onClick={() => handleBtnClick()}>Get A Quote</button>
         <div className="scroll">
-          <div className="arrow"></div>
+          <div className="arrow" onClick={() => handleArrowClick()}></div>
         </div>
       </section>
 
-      <section id="websites">
+      <section id="websites" ref={webRef}>
         <a href="http://fyc.dreamworks.com" target="_BLANK" rel="noopener noreferrer">
           <img src={dreamworks} alt="Dreamworks FYC 2021" />
         </a>
